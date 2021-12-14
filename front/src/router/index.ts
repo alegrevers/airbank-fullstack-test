7ee4359ed71eedb8e49694dfe46e80c18089a32e
gabrielalegrete <gabrielalegrete1@gmail.com>
2021-12-14 01:39:56 -0300

    front

---
+++ front/src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'transactions',
+    component: () => import('@/views/Transactions.vue')
+  },
+  {
+    path: '/transactions/:id',
+    name: 'transactions-view',
+    component: () => import('@/views/TransactionsView.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('@/views/NotFound.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
